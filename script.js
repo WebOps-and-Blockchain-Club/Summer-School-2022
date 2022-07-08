a313e56493f6876212d3e659d0caf68d58e120af
Rizan <90903715+rizan21@users.noreply.github.com>
2022-07-08 17:38:36 +0530

    Update script.js
    fill required fields 🤦🏽

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function showOnConsole(){
-    console.log(`Name         : "${document.getElementById('name').value}"`);
-    console.log(`College Name : "${document.getElementById('cname').value}"`);
-    console.log(`Email        : "${document.getElementById('email').value}"`);
-    console.log(`Mobile No    : "${document.getElementById('phno').value}"`);
+    let name=document.getElementById('name').value;
+    let email=document.getElementById('email').value;
+    if(name=="")
+        alert("wHERE nAME????");
+    else if(email=="")
+        alert("wHERE eMAIL????");
+    else{
+        console.log(`Name         : "${name}"`);
+        console.log(`College Name : "${document.getElementById('cname').value}"`);
+        console.log(`Email        : "${email}"`);
+        console.log(`Mobile No    : "${document.getElementById('phno').value}"`);
+    }
+    
 }
