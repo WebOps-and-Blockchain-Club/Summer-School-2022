d56a5d31420240381f8e7cb59b18714b8684e8f2
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-07-09 16:44:42 +0530

    google form task

---
+++ Google_Form/googleform.js
@@ -0,0 +1,19 @@
+function myfunction(){
+let x = document.getElementsByClassName("answer")
+// for(var i = 0; i<x.length; i++)
+console.log(0)
+console.log(x[1])
+console.log(1)
+// console.log(x)
+}
+
+// console.log("done")
+// console.log(x)
+
+// var x = document.getElementById("answer");
+//   var text = "";
+//   var i;
+//   for (i = 0; i < x.length ;i++) {
+//     text += x.elements[i].value + "<br>";
+//   }
+//   document.getElementById("demo").innerHTML = text;
