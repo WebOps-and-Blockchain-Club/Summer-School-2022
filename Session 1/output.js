906c60f305fb1ad7a47d7544b214e245819600d5
SamriddhaSaha <108786655+SamriddhaSaha@users.noreply.github.com>
2022-07-09 13:15:20 +0530

    Add files via upload

---
+++ Session 1/output.js
@@ -0,0 +1,11 @@
+function printname(){
+
+   let s1= document.getElementById("1").value;
+   console.log(s1);
+   let s2= document.getElementById("2").value;
+   console.log(s2);
+   let s3= document.getElementById("3").value;
+   console.log(s3);
+   let s4= document.getElementById("4").value;
+   console.log(s4);
+}
