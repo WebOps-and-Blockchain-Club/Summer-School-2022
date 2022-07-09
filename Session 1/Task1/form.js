54d8c8d48cc9135ff0767f34ce0ae3697e9ff8e6
ajaykrishna1 <na18b011@smail.iitm.ac.in>
2022-07-09 20:14:36 +0530

    Task 1 folder uploaded in Session 1 folder

---
+++ Session 1/Task1/form.js
@@ -0,0 +1,16 @@
+
+function changingHeading(){
+  
+    var name1=document.getElementById("namef").value;
+    var name2=document.getElementById("Collegename").value;
+    var name3=document.getElementById("Email").value;
+    var name4=document.getElementById("mobile").value;
+    
+    console.log("Name",name1);
+    console.log("Collegename",name2);
+    console.log("Email",name3);
+    console.log("mobile",name4);
+
+
+     
+}
