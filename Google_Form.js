100ecadd3aecce512918b623a3cf4301a8974c68
Deva Pradha Abinidhi R <102250302+Deva1292@users.noreply.github.com>
2022-07-11 00:04:10 +0530

    Add files via upload

---
+++ Google_Form.js
@@ -0,0 +1,8 @@
+
+function Print() {
+    var name = document.getElementById("names").value;
+    var college = document.getElementById("college").value;
+    var email= document.getElementById("email").value;
+    var mobile = document.getElementById("mobile").value;
+    console.log("Name:" + name , "College Name:" + college , "Email:" + email , "Mobile Number:" + mobile);
+}
