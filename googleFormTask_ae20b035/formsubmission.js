5d6f69e9489b842365451eb9c93aa549de633a22
anshadkonnola <ae20b035@smail.iitm.ac.in>
2022-07-10 16:54:31 +0530

    Finished google form task

---
+++ googleFormTask_ae20b035/formsubmission.js
@@ -0,0 +1,11 @@
+function toConsole(){
+    let name=document.forms["myForm"]["name"].value;
+    let collegeName=document.forms["myForm"]["college"].value;
+    let email=document.forms["myForm"]["email"].value;
+    let mobNo=document.forms["myForm"]["mob"].value;
+    console.log("Name: ",name);
+    console.log("College Name: ",collegeName);
+    console.log("E-mail: ",email);
+    console.log("Mob.No: ",mobNo);
+
+  }
