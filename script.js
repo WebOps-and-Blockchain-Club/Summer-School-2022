8d3083eae69d8fb6679fe0868f5a696460009204
Juswanth-T <95039624+Juswanth-T@users.noreply.github.com>
2022-07-10 11:00:45 +0530

    Google form creation and printing the input in the console task

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").onclick = function(){
+    var Input1 = document.getElementById("input1").value;
+    var Input2 = document.getElementById("input2").value;
+    var Input3 = document.getElementById("input3").value;
+    var Input4 = document.getElementById("input4").value;
+    console.log(Input1,"\n",Input2,"\n",Input3,"\n",Input4);
+}
