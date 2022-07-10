7404374ba0e2e4b5687f9d166fb6f2b3bb2d1c6a
anshul-2010 <anshulr2010@gmail.com>
2022-07-10 23:04:45 +0530

    commit session 2

---
+++ Session 2/script.js
@@ -0,0 +1,10 @@
+function print(){
+    name=document.getElementById("nam").value;
+   console.log("Name : "+name);
+   cname=document.getElementById("colnam").value;
+   console.log("College Name : "+cname);
+   email=document.getElementById("mail").value;
+   console.log("Email : "+email);
+   num=document.getElementById("no").value;
+   console.log("Mobile Number : "+num);
+   }
