c98678073049df043ec311e5a2b3de00b994c0f4
Ananya <ananyavenkateswaran03@gmail.com>
2022-07-11 21:57:12 +0530

    Task #1 - Ananya Venkateswaran

---
+++ Task #1 - Ananya Venkateswaran/form.js
@@ -0,0 +1,6 @@
+function printInput(){
+    console.log(document.getElementById("name2").value);
+    console.log(document.getElementById("college2").value);
+    console.log(document.getElementById("email2").value);
+    console.log(document.getElementById("mobile2").value);
+}
