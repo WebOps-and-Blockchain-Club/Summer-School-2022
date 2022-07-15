98649ea9634229634eedbcba2319eda3b70d0a57
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-07-15 17:46:52 +0530

    conflits resolved

--- Google_Form/googleform.js
+++ Google_Form/googleform.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function googleformanswers(){
 let username = document.getElementById("username").value
 let collegename = document.getElementById("collegename").value
@@ -9,15 +8,3 @@ console.log(collegename)
 console.log(useremail)
 console.log(usermobile)
 }
-=======
-function googleformanswers(){
-let username = document.getElementById("username").value
-let collegename = document.getElementById("collegename").value
-let useremail = document.getElementById("useremail").value
-let usermobile = document.getElementById("usermobile").value
-console.log(username)
-console.log(collegename)
-console.log(useremail)
-console.log(usermobile)
-}
->>>>>>> bf48e58 (windows -> wsl)
--- NetAccessIITM/netaccess.bat
+++ NetAccessIITM/netaccess.bat
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 TIMEOUT 3
 C:
 can place this file anywhere
 cd {D:\summer_school\WebOps-SummerSchool2022\Session3\NetAccessIITM}
 npm start
 
-=======
-TIMEOUT 3
-C:
-can place this file anywhere
-cd {D:\summer_school\WebOps-SummerSchool2022\Session3\NetAccessIITM}
-npm start
-
->>>>>>> bf48e58 (windows -> wsl)
 cmd /k
